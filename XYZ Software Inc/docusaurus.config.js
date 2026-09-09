058a2cf66d61d25882f103785b078bf328ae9db5
Kaustav Chatterjee <kchatterjee@prismhr.com>
2026-09-09 21:04:56 +0530

    Configure GitHub Pages Deployment

--- XYZ Software Inc/docusaurus.config.js
+++ XYZ Software Inc/docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'XYZ Software Inc',
+  tagline: 'Software built for your business',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://kaustav-tw.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docs-as-code-training/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Kaustav-TW',
+  projectName: 'docs-as-code-training',
 
   onBrokenLinks: 'throw',
 
@@ -54,7 +54,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Kaustav-TW/docs-as-code-training/tree/main/XYZ%20Software%20Inc/',
         },
         blog: {
           showReadingTime: true,
@@ -65,7 +65,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Kaustav-TW/docs-as-code-training/tree/main/XYZ%20Software%20Inc/blog/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
@@ -89,7 +89,7 @@ const config = {
         respectPrefersColorScheme: true,
       },
       navbar: {
-        title: 'My Site',
+        title: 'XYZ Software Inc',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -99,7 +99,7 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'docsSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Help',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
@@ -116,7 +116,7 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
+                label: 'Help',
                 to: '/docs/intro',
               },
             ],
@@ -152,7 +152,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} XYZ Software Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
